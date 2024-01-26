8ec5c2160d9f38a14d70c22ed5d39b57a3fda1f0
TOKISAKIX\21168 <2116884726@qq.com>
2024-01-26 09:20:42 +0800

    add logger

--- image_classify/train.py
+++ image_classify/train.py
@@ -9,15 +9,21 @@ from load_config import load_config
 from data import get_dataset
 from dataloader import get_dataloader
 from model import Model
+from logger import Logger
 
 CONFIG        = load_config()
 CUDA          = CONFIG["cuda"]
+LOG_CONFIG    = CONFIG["log"]
+LOG_ROOT      = LOG_CONFIG["root"]
+SAVE_NUM      = LOG_CONFIG["save_num"]
+logger        = Logger(LOG_ROOT, SAVE_NUM)
+
 TRAIN_CONFIG  = CONFIG["train"]
 LEARNING_RATE = TRAIN_CONFIG["learning_rate"]
 EPOCHS        = TRAIN_CONFIG["epochs"]
 SHOW_CONFIG   = CONFIG["show"]
-ACC_IMG       = SHOW_CONFIG["acc_img"]
-LOSS_IIMG     = SHOW_CONFIG["loss_img"]
+ACC_IMG       = os.path.join(logger.root, SHOW_CONFIG["acc_img"])
+LOSS_IIMG     = os.path.join(logger.root, SHOW_CONFIG["loss_img"])
 
 def test(model, test_dataloader):
     cnt = 0
@@ -37,7 +43,7 @@ def test(model, test_dataloader):
     acc =  cnt / tot
     return acc
 
-def train(model, optimizer, ceriterion, train_dataloader, test_dataloader):
+def train(model, optimizer, ceriterion, train_dataloader, test_dataloader, logger):
     start = perf_counter()
     tot_loss = 0
     epoch_list = []
@@ -61,18 +67,18 @@ def train(model, optimizer, ceriterion, train_dataloader, test_dataloader):
         acc = test(model, test_dataloader) * 100
         time  = perf_counter() - start
         start = perf_counter()
-        print(f"[INFO] Epoch:{epoch} Loss:{loss:.6f} Acc:{acc:.2f}% Time:{time:.2f}s")
+        logger.info("\n------")
+        logger.info(f"Epoch:{epoch:3d} Loss:{loss:10.6f} Acc:{acc:6.2f}% Time:{time:6.2f}s.")
+        logger.save_model(model, f"Epoch_{epoch}.pth")
+        logger.info(f"Save model as Epoch_{epoch}.pth")
         epoch_list.append(epoch)
         loss_list.append(loss)
         acc_list.append(acc)
 
-    print("[INFO] Finished training!")
+    logger.info("Finished training!")
     return  epoch_list, loss_list, acc_list
 
 def draw(epoch_list, loss_list, acc_list):
-    if not os.path.isdir("log"):
-        os.mkdir("log")
-
     plt.plot(epoch_list, loss_list, label="Loss")
     plt.title("Loss Img")
     plt.legend()
@@ -90,17 +96,21 @@ def draw(epoch_list, loss_list, acc_list):
 
 
 if __name__ == "__main__":
+    logger.info("Logger init.")
 
     train_dataset = get_dataset(CONFIG, mod="train")
     test_dataset  = get_dataset(CONFIG, mod="test")
 
     train_dataloader = get_dataloader(CONFIG, train_dataset, mod="train")
     test_dataloader  = get_dataloader(CONFIG, test_dataset,  mod="test")
+    logger.info("Load data.")
 
     model = Model()
     model = model.cuda() if CUDA else model
+    logger.info("Build model.")
+
     optimizer  = optim.Adam(model.parameters(), lr=LEARNING_RATE)
     ceriterion = nn.CrossEntropyLoss()
 
-    epoch_list, loss_list, acc_list = train(model, optimizer, ceriterion, train_dataloader, test_dataloader)
+    epoch_list, loss_list, acc_list = train(model, optimizer, ceriterion, train_dataloader, test_dataloader, logger)
     draw(epoch_list, loss_list, acc_list)
